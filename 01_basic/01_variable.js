80f1ad5c254ddaeecce171c04e39fddef98bdde7
Ashish-rj-1108 <118763741+Ashish-rj-1108@users.noreply.github.com>
2025-03-09 05:41:16 +0000

    variables

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,12 @@
+var ashish = 1111
+let ranjan = 2222
+let asd = 'ashish ranjan'
+
+const thisIsmy_constant = 'software engineer'
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([ashish , ranjan , thisIsmy_constant , asd])
